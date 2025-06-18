d82476fd89cc14c4f9285d40f8958f6ea9550451
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-19 00:16:03 +0200

    Update ejecutar_plus_30.sh

--- ejecutar_plus_30.sh
+++ ejecutar_plus_30.sh
@@ -21,7 +21,7 @@ sudo rm /home/pi/RXF_DMRPLUS.desktop
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh cerrar_plus_30.sh'" /home/pi/Abrir_MMDVMPLUS
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;sudo sh cerrar_plus_30.sh'" /home/pi/Abrir_MMDVMPLUS
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/DMRPLUS_ON.png" /home/pi/Abrir_MMDVMPLUS
 sed -i "10c Name[es_ES]=Cerrar DMR+" /home/pi/Abrir_MMDVMPLUS
 sed -i "6c MMDVMPLUS=ON" /home/pi/status.ini
