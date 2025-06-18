3ccb137a3f336bbc554f09dbeb295faf5bb5df5c
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-18 13:26:47 +0200

    1

--- nota_version.sh
+++ nota_version.sh
@@ -27,7 +27,7 @@ echo "   Jonathan Naylor G4KLX, David Palm PA7LIM, José Uribe (Andy) CA6JAU, ON
 echo "   Torsten Schultze DG1HT, Estefan Reiman DG8FAC, Manuel EA7EE y Kurt Moraw DJ0ABR." 
 echo ""
 echo -n "${AMARILLO}"
-echo "   Agradecer la colaboración a: EA3ES, EA3EG, EA3HVF, EA3FAB, EA4AOJ y EA1IVQ."
+echo "   "
 echo ""
 echo "\33[1;31m   Recomendación de no modificar total o parcialmente el contenido de esta imagen."
 echo "\33[1;31m   Podria dañar algún fichero del sistema y el funcionamiento de la misma."
