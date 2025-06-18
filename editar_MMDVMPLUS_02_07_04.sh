91407c5b842450c2829bfadb4c3ff9601e149c47
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-18 20:50:39 +0200

    Update editar_MMDVMPLUS_02_07_04.sh

--- editar_MMDVMPLUS_02_07_04.sh
+++ editar_MMDVMPLUS_02_07_04.sh
@@ -110,8 +110,16 @@ echo "${CIAN}   8)${GRIS} Puerto para placa NTH/ZUM en arduino y Pincho Low Cost
 echo "${CIAN}   9)${GRIS} Puerto para DVMEGA + Bluestack conectado por USB a Raspberry Pi(ttyUSB0)${AMARILLO}"
 echo -n "                            - "
 
-mode=$(awk "NR==51" /home/pi/MMDVMHost/$DIRECTORIO)
-echo "$mode"
+remoteport=$(awk '
+/^\[Modem\]/ {in_section=1; next}
+/^\[/ {in_section=0}
+in_section && /^UartPort=/ {
+    split($0, a, "=")
+    print a[2]
+    exit
+}' /home/pi/MMDVMHost/$DIRECTORIO)
+echo -n "${CIAN}  9)${GRIS} Valor RemotePort      - ${AMARILLO}${remoteport}\33[1;37m"
+echo ""
 
 echo -n "${CIAN}  10)${GRIS} Modificar ID          - ${AMARILLO}"
 idd=`grep -n "Id=" /home/pi/MMDVMHost/$DIRECTORIO`
