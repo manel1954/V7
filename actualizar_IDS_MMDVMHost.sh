abf8f9a73a73276333c45c3a805729e09e12e3d4
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-18 12:36:04 +0200

    Update actualizar_IDS_MMDVMHost.sh

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 clear
-echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS adadadadad >>>>>"
+echo "\33[1;32m ACTUALIZANDO IDS INDICATIVOS >>>>>"
 sleep 10
 
 #var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
