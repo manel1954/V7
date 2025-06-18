8d785e8e88de1187678c6fd28d65be5a4530339e
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-18 11:46:34 +0200

    Update nota_version_02_07_05.sh

--- nota_version_02_07_05.sh
+++ nota_version_02_07_05.sh
@@ -17,7 +17,7 @@ echo "   ***********************************************************************
 echo ""
 echo "\33[1;32m   Imagen multi plataforma para \33[1;33mRADIO AFICIONADOS."
 echo "\33[1;32m"
-echo "   Imagen Linux basada en Debian Stretch, creada por: EA3EIZ & EA3EG."
+echo "   Imagen Linux basada en Debian Stretch, creada por: EA3EIZ"
 echo ""
 echo "\33[1;33m   - Utilizar adecuadamente los puertos de los correspondientes Módems."
 echo ""
