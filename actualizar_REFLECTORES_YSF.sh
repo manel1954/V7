7300029b3c8ffd34320f46b5df057f579871893f
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-06-19 23:08:54 +0200

    1

--- actualizar_REFLECTORES_YSF.sh
+++ actualizar_REFLECTORES_YSF.sh
@@ -1,14 +1,20 @@
 ﻿#!/bin/bash
+#Colores
 ROJO="\033[1;31m"
 VERDE="\033[1;32m"
 BLANCO="\033[1;37m"
 AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
+MARRON="\33[38;5;138m"
 
 			cd /home/pi/YSFClients/YSFGateway
-			sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
-            clear
+			#sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
+            
+			curl https://dvref.com/downloads/YSFHosts-resolved.txt > /home/pi/YSFClients/YSFGateway/YSFHosts.txt
+			
+			
+			clear
 			echo "${VERDE}*********************************"
 					echo "* ACTUALIZANDO REFLECTORES YSF  *"
 					echo "*********************************"
